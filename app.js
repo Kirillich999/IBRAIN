d434e01e5d6561914c5207db8ec73f7a7b3cf44b
kirillich999 <bakurfu32@yandex.ru>
2024-04-10 20:37:42 +0500

    3 section adaption+js

---
+++ app.js
@@ -0,0 +1,13 @@
+document. addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click" , function() {
+        document.querySelector ("header").classList.toggle("open");
+    });
+
+});
+
+document. addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger-menu").addEventListener("click" , function() {
+        document.querySelector ("header").classList.toggle("open");
+    });
+
+});
